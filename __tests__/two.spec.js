738a53fdd0c8f1200bfda17bcef3755d8e812fdf
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2022-12-01 17:02:15 +0100

    New function and tests for Day 1 - Part Two

---
+++ __tests__/two.spec.js
@@ -0,0 +1,53 @@
+import { findTopThreeElves } from '../index.js';
+
+describe('findTopThreeElves', function () {
+
+  describe('When giving an easy two elves file', function() {
+    it('should find the good max', function() {
+      // --- GIVEN
+      const inputPath = './__tests__/inputOne.txt'
+
+      // --- WHEN
+      const biggestThreeElves = findTopThreeElves(inputPath)
+
+      // --- THEN
+      expect(biggestThreeElves).toEqual({
+        topThree: [2000, 1000],
+        totalForThem: 3000
+      })
+    })
+  })
+
+  describe('When some elf have more than one portion', function () {
+    it('should find the good max', function () {
+      // --- GIVEN
+      const inputPath = './__tests__/inputTwo.txt'
+
+      // --- WHEN
+      const biggestThreeElves = findTopThreeElves(inputPath)
+
+      // --- THEN
+      expect(biggestThreeElves).toEqual({
+        topThree: [5000, 3000, 1110],
+        totalForThem: 9110,
+      })
+    })
+  })
+
+  describe('When using the real input!', function () {
+    it('should find the good max', function () {
+      // --- GIVEN
+      const inputPath = './__tests__/realInput.txt'
+
+      // --- WHEN
+      const biggestThreeElves = findTopThreeElves(inputPath)
+
+      // --- THEN
+      expect(biggestThreeElves).toEqual({
+        topThree: [70698, 69773, 66172],
+        totalForThem: 206643,
+      })
+    })
+  })
+
+})
--- index.js
+++ index.js
@@ -20,3 +20,25 @@ export function findBiggestElf(inputPath) {
     totalRation: max,
   }
 }
+
+export function findTopThreeElves(inputPath) {
+  const content = fs.readFileSync(inputPath, 'utf-8')
+  const result = computeAndSortRations(content)
+  return {
+    topThree: result,
+    totalForThem: result.reduce((accu, current) => accu + current, 0)
+  }
+}
+
+export function computeAndSortRations(content) {
+  const elves = content.split('\n\n')
+  const totalByElf = elves.map(elfRations => elfRations.split('\n').map(Number).reduce((accu, current) => {
+    if (!current) {
+      return accu
+    }
+    return accu + current
+  }, 0))
+  return totalByElf.sort(function(a, b) {
+    return a - b;
+  }).reverse().slice(0, 3)
+}
