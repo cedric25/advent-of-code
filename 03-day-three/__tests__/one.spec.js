faae3503d5eee31ad9c970eec3fecca81ff9489c
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2022-12-02 13:44:35 +0100

    Prepare day 3

---
+++ 03-day-three/__tests__/one.spec.js
@@ -0,0 +1,16 @@
+import { computeSomething } from '../index.js'
+
+describe('computeSomething', function () {
+  describe('When giving an easy file', function () {
+    it('should get the right result', function () {
+      // --- GIVEN
+      const inputPath = './__tests__/inputOne.txt'
+
+      // --- WHEN
+      const totalScore = computeSomething(inputPath)
+
+      // --- THEN
+      expect(totalScore).toEqual(-1)
+    })
+  })
+})
---
+++ 03-day-three/index.js
@@ -0,0 +1,11 @@
+import path from 'node:path'
+import fs from 'node:fs'
+import * as url from 'node:url'
+
+export function computeSomething(inputPath) {
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+  const content = fs.readFileSync(path.join(__dirname, inputPath), 'utf-8')
+  const rounds = content.split('\n')
+  console.log('rounds', rounds)
+  return -1
+}
