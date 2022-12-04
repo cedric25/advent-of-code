249acfe4eb92ac1871eb9576278d6b3691f1ba71
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2022-12-04 09:28:09 +0100

    Prepare for day four

---
+++ 04-day-four/index.js
@@ -0,0 +1,11 @@
+import path from 'node:path'
+import fs from 'node:fs'
+import * as url from 'node:url'
+
+export function computeSomething(inputPath) {
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+  const content = fs.readFileSync(path.join(__dirname, inputPath), 'utf-8').trim()
+  let lines = content.split('\n')
+  console.log('lines', lines)
+  return -1
+}
