b536b58e52c3e5f996913f355e174d736fb97a4e
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2022-12-11 01:04:23 +0100

    Create readFileTrimEnd() helper

---
+++ helpers/helpers.js
@@ -0,0 +1,8 @@
+import url from 'node:url'
+import fs from 'node:fs'
+import path from 'node:path'
+
+export function readFileTrimEnd(inputPath) {
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+  return fs.readFileSync(path.join(__dirname, '../', inputPath), 'utf-8').trimEnd()
+}
