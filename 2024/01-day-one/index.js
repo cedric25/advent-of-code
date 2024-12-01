72c45198e53c671f037c40dccc2935f4277ab8ea
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2024-12-01 22:14:20 +0100

    Day 1 star 2

--- 2024/01-day-one/index.js
+++ 2024/01-day-one/index.js
@@ -32,5 +32,33 @@ export function calculateDistance(inputPath) {
   console.log('sumDistance', sumDistance)
 }
 
+export function calculateDistanceStarTwo(inputPath) {
+  const content = readFileTrimEnd(inputPath)
+  const lines = content.split('\n')
+
+  const firstList = []
+  const secondList = []
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i]
+    firstList.push(Number(line.split('   ')[0]))
+    secondList.push(Number(line.split('   ')[1]))
+  }
+  // console.log('firstList', firstList)
+  // console.log('secondList', secondList)
+
+  let sum = 0
+  for (let i = 0; i < firstList.length; i++) {
+    // console.log('firstList[0]', firstList[0])
+    const timesInListTwo = secondList.filter(item => item === firstList[i]).length;
+    // console.log('timesInListTwo', timesInListTwo)
+    sum += firstList[i] * timesInListTwo
+  }
+  console.log('sum', sum)
+}
+
 // calculateDistance('./2024/01-day-one/inputs/inputOne.txt')
-calculateDistance('./2024/01-day-one/inputs/realInputOne.txt')
+// calculateDistance('./2024/01-day-one/inputs/realInputOne.txt')
+
+// calculateDistanceStarTwo('./2024/01-day-one/inputs/inputOne.txt')
+calculateDistanceStarTwo('./2024/01-day-one/inputs/realInputOne.txt')
