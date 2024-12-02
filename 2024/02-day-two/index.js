54f15cc2caf117a6a8a7ce1d5d38f18b7e33d089
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2024-12-02 22:26:09 +0100

    Day 2 star 2

--- 2024/02-day-two/index.js
+++ 2024/02-day-two/index.js
@@ -4,7 +4,7 @@ import { readFileTrimEnd } from '../../helpers/helpers.js';
  * node ./02-day-two/index.js
  */
 
-export function countSafeReports(inputPath) {
+function countSafeReports(inputPath) {
   const content = readFileTrimEnd(inputPath)
   const lines = content.split('\n')
 
@@ -43,16 +43,79 @@ export function countSafeReports(inputPath) {
         break
       }
     }
+  }
+
+  console.log('countSafeReports', countSafeReports)
+}
+
+// --- Star 2
+
+function countSafeReportsStarTwo(inputPath) {
+  const content = readFileTrimEnd(inputPath)
+  const lines = content.split('\n')
+
+  let countSafeReports = 0
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i]
+    const levels = line.split(' ').map(Number);
+    const isReportSafe = isLineSafe(levels)
     if (isReportSafe === true) {
       countSafeReports++
+      continue
+    }
+    // Untouched line is unsafe
+    for (let j = 0; j < levels.length; j++) {
+      const lineWithoutOneLevel = immutableSplice(levels, j)
+      const isReportSafe = isLineSafe(lineWithoutOneLevel)
+      if (isReportSafe === true) {
+        countSafeReports++
+        break
+      }
     }
   }
 
   console.log('countSafeReports', countSafeReports)
 }
 
-// countSafeReports('./2024/02-day-two/inputs/inputOne.txt')
-countSafeReports('./2024/02-day-two/inputs/realInputOne.txt')
+function isLineSafe(levels) {
+  let isIncreasing = null
+  let isDecreasing = null
+  for (let j = 0; j < levels.length; j++) {
+    if (levels[j + 1] === undefined) {
+      continue
+    }
+    if (levels[j + 1] > levels[j]) {
+      isIncreasing = true;
+      if (isDecreasing) {
+        return false
+      }
+    }
+    if (levels[j + 1] < levels[j]) {
+      isDecreasing = true;
+      if (isIncreasing) {
+        return false
+      }
+    }
+    if (Math.abs(levels[j + 1] - levels[j]) > 3) {
+      return false
+    }
+    if (levels[j + 1] === levels[j]) {
+      return false
+    }
+  }
+  return true
+}
+
+function immutableSplice(arr, start) {
+  return [
+    ...arr.slice(0, start),
+    ...arr.slice(start + 1)
+  ];
+}
 
 // countSafeReports('./2024/02-day-two/inputs/inputOne.txt')
 // countSafeReports('./2024/02-day-two/inputs/realInputOne.txt')
+
+// countSafeReportsStarTwo('./2024/02-day-two/inputs/inputOne.txt')
+countSafeReportsStarTwo('./2024/02-day-two/inputs/realInputOne.txt')
