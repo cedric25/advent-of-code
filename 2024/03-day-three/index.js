29bb1611257adfb671531b407e127474ea51fc76
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2024-12-03 23:19:39 +0100

    Day 3 star 2

--- 2024/03-day-three/index.js
+++ 2024/03-day-three/index.js
@@ -4,9 +4,18 @@ import { readFileTrimEnd } from '../../helpers/helpers.js'
  * node ./03-day-three/index.js
  */
 
-function sumOfMul(inputPath) {
+function sumOfMulStarOne(inputPath) {
   const content = readFileTrimEnd(inputPath)
 
+  const sum = sumOfMul(content)
+
+  // Log useful for star 1
+  console.log('sum', sum)
+
+  // return sum
+}
+
+function sumOfMul(content) {
   const regex = /mul\(\d+,\d+\)/g
 
   let match
@@ -22,11 +31,35 @@ function sumOfMul(inputPath) {
     sum += Number(digits[1]) * Number(digits[2])
   }
 
-  console.log('sum', sum)
+  return sum
 }
 
-// sumOfMul('./2024/03-day-three/inputs/inputOne.txt')
-sumOfMul('./2024/03-day-three/inputs/realInputOne.txt')
+// --- Star 2
 
-// sumOfMul('./2024/03-day-three/inputs/inputOne.txt')
-// sumOfMul('./2024/03-day-three/inputs/realInputOne.txt')
+// 3203510 Too low
+// 68015012 Too low
+// 74838033 Good!
+
+function sumOfMulStarTwo(inputPath) {
+  let content = readFileTrimEnd(inputPath)
+  content = content.replaceAll('\n', '') // !!! :(
+
+  const beginning = /(.*?)don't/.exec(content)
+  let sum = sumOfMul(beginning[1])
+
+  const inBetweenRegex = /do(?!n't)(.*?)don\'t/g
+  let match
+  while ((match = inBetweenRegex.exec(content)) !== null) {
+    sum += sumOfMul(match[1])
+  }
+
+  console.log(sum)
+}
+
+// sumOfMulStarOne('./2024/03-day-three/inputs/inputOne.txt')
+// sumOfMulStarOne('./2024/03-day-three/inputs/realInputOne.txt')
+
+// sumOfMulStarTwo('./2024/03-day-three/inputs/inputStar2.txt')
+// sumOfMulStarTwo('./2024/03-day-three/inputs/inputStar2_02.txt')
+// sumOfMulStarTwo('./2024/03-day-three/inputs/inputStar2_03.txt')
+sumOfMulStarTwo('./2024/03-day-three/inputs/realInputOne.txt')
